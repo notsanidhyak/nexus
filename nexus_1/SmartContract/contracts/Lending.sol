008f656b6b88612ab45aaad5fcebcad72de71a39
sanidhyak <kumarsanidhya.31@gmail.com>
2023-02-05 00:04:54 +0530

    Smart Contract Uploaded
    Files added

---
+++ nexus_1/SmartContract/contracts/Lending.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Lending {
+    using Counters for Counters.Counter;
+    Counters.Counter private B_ID_Counter;
+    
+    struct Borrower{
+        address payable B_address;
+        uint256 amount;
+        bool hasRecieved;
+    }
+
+    event _Borrow(uint256 indexed B_id, address indexed B_address, uint256 indexed _amount);
+    event _lend(uint256 indexed B_id, address indexed L_address);
+
+    //mapping from B_ID to Borrower
+    mapping(uint256 => Borrower) public B_data;
+    
+    function borrow(address payable _address, uint256 _amount) public {
+        B_data[B_ID_Counter.current()] = Borrower(_address, _amount, false);
+        emit _Borrow(B_ID_Counter.current(), _address, _amount);
+        B_ID_Counter.increment();
+    }
+
+    function lend(uint256 B_ID) external payable returns(bool){
+        require(msg.sender != B_data[B_ID].B_address, "Lender and borrower cant be same.");
+        require(!B_data[B_ID].hasRecieved,"Borrow's request has already been fulfilled !!" );
+        require(msg.value == B_data[B_ID].amount ,"Pay Exact Value");
+        B_data[B_ID].B_address.transfer(msg.value);
+        B_data[B_ID].hasRecieved = true;
+        emit _lend(B_ID, msg.sender);
+        return true;
+    }
+    
+}
