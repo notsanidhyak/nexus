e1990f1a73dcb1ed556585a6ebfe0e2cabb0c546
sanidhyak <kumarsanidhya.31@gmail.com>
2023-02-05 04:33:04 +0530

    frontend updated

--- nexus_1/Nexus_frontend/server.js
+++ nexus_1/Nexus_frontend/server.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/landingpage.html"));
 })
 
+
+
 app.get("/minted", (req, res) => {
     res.sendFile(path.join(__dirname + "/nft_minted.html"));
 })
@@ -25,6 +27,23 @@ app.get("/borrow", (req, res) => {
 app.get("/lendPage", (req, res) => {
     res.sendFile(path.join(__dirname + "/lendpage.html"));
 })
+
+app.get("/profile", (req, res) => {
+    res.sendFile(path.join(__dirname + "/profile.html"));
+})
+
+app.get("/who", (req, res) => {
+    res.sendFile(path.join(__dirname + "/who.html"));
+})
+
+app.get("/how", (req, res) => {
+    res.sendFile(path.join(__dirname + "/how.html"));
+})
+
+app.get("/why", (req, res) => {
+    res.sendFile(path.join(__dirname + "/why.html"));
+})
+
 
 // app.get("/verified", (req, res) => {
 //     res.sendFile(path.join(__dirname + "/verified.html"));
